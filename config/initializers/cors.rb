f0b6a81973f81d81574e5b56db938970ef6868a6
alanbanks229 <alanbanks229@gmail.com>
2020-05-19 00:43:07 -0400

    . . .

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,17 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
+    # allow do
+    #   origins 'https://current-trends-app.herokuapp.com/'
+    #   #initially http://localhost:3000
+    # #  on deployment origins needs to be changed to current_trends_app.heroku.com/blablal
+    #   resource '*',
+    #     headers: :any,
+    #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    #     credentials: true
+    # end
+
     allow do
-      origins 'https://current-trends-app.herokuapp.com/'
-      #initially http://localhost:3000
-    #  on deployment origins needs to be changed to current_trends_app.heroku.com/blablal
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
+      origins '*'
+      resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
     end
+
   end
