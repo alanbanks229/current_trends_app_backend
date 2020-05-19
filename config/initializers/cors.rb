7df1f4917a76dd413eee91b1734f79ae791dabe5
alanbanks229 <alanbanks229@gmail.com>
2020-05-19 00:29:13 -0400

    you gotta love those syntax mistakes, changed origins from app-api to app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'https://current-trends-app-api.herokuapp.com'
+      origins 'https://current-trends-app.herokuapp.com/'
       #initially http://localhost:3000
     #  on deployment origins needs to be changed to current_trends_app.heroku.com/blablal
       resource '*',
