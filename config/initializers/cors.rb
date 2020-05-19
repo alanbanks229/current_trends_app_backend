703ded8052ebcdb431cf2036ccf662f69df8ee74
alanbanks229 <alanbanks229@gmail.com>
2020-05-19 01:45:32 -0400

    switched all instances of localhost:3000 with herokuapp url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # end
 
     allow do
-      origins 'http://localhost:3000'
+      origins 'https://current-trends-app.herokuapp.com'
   
       resource '*',
         headers: :any,
