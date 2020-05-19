42295a8bf9eaf8e19319ca8af3fb0e3a37d005fe
alanbanks229 <alanbanks229@gmail.com>
2020-05-19 00:23:46 -0400

    hopefully fixed cors bug, could not use wildcard * for cors header since i'm using cookies potentially

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins '*'
+      origins 'https://current-trends-app-api.herokuapp.com'
+      #initially http://localhost:3000
+    #  on deployment origins needs to be changed to current_trends_app.heroku.com/blablal
       resource '*',
         headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
     end
   end
