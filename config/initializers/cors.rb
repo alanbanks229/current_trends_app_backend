5a52b50617e09789797619354a67158781120d87
alanbanks229 <alanbanks229@gmail.com>
2020-06-09 15:01:18 -0400

    edited code for localhost server usage, due to changes in NEWSAPI developer plan

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # end
 
     allow do
-      origins 'https://current-trends-app.herokuapp.com'
+      origins 'http://localhost:3000'
   
       resource '*',
         headers: :any,
