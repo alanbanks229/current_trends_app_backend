8e4b4a9a6ce4a0e419e2a68dd878cce67e14deb1
alanbanks229 <alanbanks229@gmail.com>
2020-06-09 15:05:33 -0400

    added a change in config/application.rb, Access-Control-Allow-Origin changed to localhost:3000

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module CurrentTrendsApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'https://current-trends-app.herokuapp.com',
+      'Access-Control-Allow-Origin' => 'http://localhost:3000',
       'Access-Control-Request-Method' => %w{GET POST DELETE}.join(",")
     }
     # Settings in config/environments/* take precedence over those specified here.
